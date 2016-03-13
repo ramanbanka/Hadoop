61e4840e4e2d5306c055e650b8c8b8cf6836605e
Raman Banka <ramanbanka123@gmail.com>
2016-03-13 16:51:25 +0530

    Renamed the files for the steps and added a new directory which contains the fils for making the installtion easy.

---
+++ cfg/single-to-multi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+ echo "This script must be run as root";
+ exit 1;
+fi
+
+fromDir=multi-node
+toDir=/home/hduser/hadoop/etc/hadoop/
+
+cp $fromDir/hdfs-site.xml $toDir
+cp $fromDir/yarn-site.xml $toDir
+cp $fromDir/mapred-site.xml $toDir
+cp $fromDir/masters $toDir
+cp $fromDir/slaves $toDir
+cp $fromDir/hosts /etc/
+
